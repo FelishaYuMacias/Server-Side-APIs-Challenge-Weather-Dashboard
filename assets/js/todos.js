f492cd04a086ea879d8e374d1377149d74f47426
Felisha <felisha.j.yu@gmail.com>
2022-10-07 16:20:16 -0700

    started pseudo coding/created todo list in js

---
+++ assets/js/todos.js
@@ -0,0 +1,23 @@
+/TODO: Map out layout of page in html
+
+//TODO:Create/find form with inputs
+
+//TODO:WHEN I search for a city
+        //THEN I am presented with current and future conditions for that city and that city is added to the search history
+
+//TODO:WHEN I view current weather conditions for that city
+        //THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+
+//TODO:WHEN I view future weather conditions for that city
+    //THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+
+//TODO:WHEN I click on a city in the search history
+    //THEN I am again presented with current and future conditions for that city
+
+//TODO: Deploy site
+
+//TODO: Finalize README
+
+//TODO: Double check deployed site for bugs/issues
+
+//TODO: Submit Assignment
