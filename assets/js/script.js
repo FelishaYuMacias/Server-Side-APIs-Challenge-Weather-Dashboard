eac930b893de56e150a9671c7599bf8a2e6759ad
Felisha <felisha.j.yu@gmail.com>
2022-10-07 22:38:39 -0700

    linked jQuery stylesheet, added columns for different sections, added h2, button and textarea for search section

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,8 @@ var queryURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&
 //make the API call using fetch
 
 // fetch(queryURL)
+
+// when city is entered and button is pushed
+    //city info is saved in local storage
+    //a button is created and put in a list with city info attached
+    //city can be removed with 
